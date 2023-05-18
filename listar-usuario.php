a11c0724298334fd6c525a6ed7c69d7364e55523
André Pereira <andrelopespereira@hotmail.com>
2023-05-17 21:15:55 -0300

    Primeiro commit

---
+++ listar-usuario.php
@@ -0,0 +1,34 @@
+<h1>Listar Usuários</h1>
+<?php
+    $sql = "SELECT * FROM usuarios";
+
+    $res = $conn->query($sql);
+
+    $qtd = $res->num_rows;
+
+    if($qtd > 0){
+        echo "<table class='table table-hover table-striped table-bordered'>";
+        echo "<tr>";
+        echo "<th>#</th>";
+        echo "<th>Nome</th>";
+        echo "<th>E-mail</th>";
+        echo "<th>Nascimento</th>";
+        echo "<th>Ação</th>";
+        echo "</tr>";
+        while($row = $res->fetch_object()){
+            echo "<tr>";
+            echo "<td>".$row->id."</td>";
+            echo "<td>".$row->nome."</td>";
+            echo "<td>".$row->email."</td>";
+            echo "<td>".$row->data_nasc."</td>";
+            echo "<td>
+                <button onclick=\"location.href='?page=editar&id=".$row->id."';\" class='btn btn-secondary'>Editar</button>
+                <button onclick=\"if(confirm('Tem certeza que desejar excluir?')){location.href='?page=salvar&acao=excluir&id=".$row->id."';}else{false;} \" class='btn btn-danger'>Excluir</button>
+                </td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }else{
+        echo "<p class='alert alert-danger'>Não foi possível encontrar usuários.</p>";
+    }
+?>
